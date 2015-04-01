5521df014a5d96999b6429361a80b321f6512be2
vladimirbelinski <vladimir_belinski@hotmail.com>
2015-04-01 20:09:09 -0300

    Added a license header and the basic structure of the main class.

---
+++ Main.java
@@ -0,0 +1,18 @@
+/*
+===================================================================================================
+Name: Main.java
+Authors: Acácia dos Campos da Terra
+         Gabriel Batista Galli &
+         Vladimir Belinski
+Version: 1.0
+Copyright: Your copyleft.
+Description: Main class of Lotus, a programming language based on java.
+==================================================================================================
+*/
+
+class Main{
+  public static void main (String args[]){
+
+
+  }
+}
