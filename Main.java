63e9f340882396f837f6e0cd68b362dbf7e5254f
vladimirbelinski <vladimir_belinski@hotmail.com>
2015-04-01 22:45:07 -0300

    Update of the header (into the specifictions)

--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
 /*
 ===================================================================================================
 Name: Main.java
-Authors: Acácia dos Campos da Terra
-         Gabriel Batista Galli &
-         Vladimir Belinski
+Authors: Acácia dos Campos da Terra- terra.acacia@gmail.com
+         Gabriel Batista Galli - g7.galli96@gmail.com
+         Vladimir Belinski - vlbelinski@gmail.com
 Version: 1.0
 Copyright: Your copyleft.
 Description: Main class of Lotus, a programming language based on java.
