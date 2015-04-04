3c4e7c6fe2f330b36aa6bc8ad4ac0790fbd87703
Gabriel Galli <g7.galli96@gmail.com>
2015-04-04 17:19:32 -0300

    Now handles the case when main args vector is empty

--- fonte/Lotus.java
+++ fonte/Lotus.java
@@ -19,21 +19,29 @@ import java.io.*;
 class Lotus {
     public static void main(String[] args) throws Exception {
         int max;
-        File f = new File(args[0]);
-        Scanner s = new Scanner(f);
+        File f;
+        Scanner s;
         Interpreter lotus = new Interpreter();
         ArrayList<String> in = new ArrayList<String>();
 
-        while (s.hasNext()) {
-            in.add(s.nextLine());
-        }
-        s.close();
+        if (args.length > 0) {
+            f = new File(args[0]);
+            s = new Scanner(f);
 
-        max = in.size();
-        for (int i = 0; i < max; i++) {
-            System.out.println(in.get(i));
-            System.out.println("= " + lotus.solve(in.get(i)));
-            if (i < max - 1) System.out.println();
+            while (s.hasNext()) {
+                in.add(s.nextLine());
+            }
+            s.close();
+
+            max = in.size();
+            for (int i = 0; i < max; i++) {
+                System.out.println(in.get(i));
+                System.out.println("= " + lotus.solve(in.get(i)));
+                if (i < max - 1) System.out.println();
+            }
+        }
+        else {
+            System.out.println("No input file detected.");
         }
         /**********************************************************************/
         Variable g, d, tni;
