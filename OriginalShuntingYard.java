2f6ebc5c5d665c892a7560aaa2b7cf2122418565
Gabriel Galli <g7.galli96@gmail.com>
2015-04-12 22:23:18 -0300

    Restored initial state of the expression

--- OriginalShuntingYard.java
+++ OriginalShuntingYard.java
@@ -3,7 +3,7 @@ import java.util.*;
 class OriginalShuntingYard {
 
     public static void main(String[] args) {
-        String infix = "3 + 4 * 2 / ( 1 - 5 ) ^ 2 ^ 3 3";
+        String infix = "3 + 4 * 2 / ( 1 - 5 ) ^ 2 ^ 3";
         System.out.printf("infix:   %s%n", infix);
         System.out.printf("postfix: %s%n", infixToPostfix(infix));
     }
