d40a15d100bb762a933581b375b08dff00b276a0
Gabriel Galli <g7.galli96@gmail.com>
2015-04-17 22:49:57 -0300

    Started rewriting the Shunting Yard method

--- fonte/OriginalShuntingYard.java
+++ fonte/OriginalShuntingYard.java
@@ -8,22 +8,25 @@ class OriginalShuntingYard {
         System.out.printf("postfix: %s%n", infixToPostfix(infix));
     }
 
-    static String infixToPostfix(String infix) {
-        final String ops = "-+/*^";
+    /* Dijkstra's Shunting Yard algorithm. Taken from Rosetta Code
+	 * It needs an expression with everything separated by spaces
+	 * and that's why it calls the fixExp() monster method ;x
+	 */
+	static String infixToPostfix(String exp) {
+        final String ops = "-+/%*^";
         StringBuilder sb = new StringBuilder();
         Stack<Integer> s = new Stack<>();
+        String[] tokens = exp.split(" ");
 
-        for (String token : infix.split("\\s")) {
-            if (token.isEmpty())
-                continue;
+        for (String token: tokens) {
             char c = token.charAt(0);
             int idx = ops.indexOf(c);
 
             // check for operator
-            if (idx != -1) {
-                if (s.isEmpty())
+            if (idx != -1 && token.length() == 1) {
+                if (s.isEmpty()) {
                     s.push(idx);
-
+				}
                 else {
                     while (!s.isEmpty()) {
                         int prec2 = s.peek() / 2;
@@ -48,8 +51,9 @@ class OriginalShuntingYard {
                 sb.append(token).append(' ');
             }
         }
-        while (!s.isEmpty())
+        while (!s.isEmpty()) {
             sb.append(ops.charAt(s.pop())).append(' ');
+        }
         return sb.toString();
     }
 }
